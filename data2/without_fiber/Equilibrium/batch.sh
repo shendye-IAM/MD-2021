b859c2c9810a1cf5837e2b2d5e724562cfc5c567
Ahmadmoein <75838429+Ahmadmoein@users.noreply.github.com>
2022-02-28 01:04:33 +0100

    Add files via upload

---
+++ data2/without_fiber/Equilibrium/batch.sh
@@ -0,0 +1,21 @@
+#!/usr/local_rwth/bin/zsh
+
+### Request the memory you need for your job. You can specify this
+### in either MB (1024M) or GB (4G).
+#SBATCH --mem-per-cpu=3900M
+
+### Request 12 processes, all on a single node
+#SBATCH --nodes=2
+#SBATCH --ntasks=12
+
+### Request the time you need for execution. The full format is D-HH:MM:SS
+### You must at least specify minutes or days and hours and may add or
+### leave out any other parameters
+#SBATCH --time=1-20:00:00
+
+### Load required modules
+module load CHEMISTRY
+module load lammps
+
+### start the MPI binary
+$MPIEXEC $FLAGS_MPI_BATCH lmp_rwth -in config_Model_CSH.in
